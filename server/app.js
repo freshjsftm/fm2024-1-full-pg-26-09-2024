dd8dc1dc8ae21fa7ef6cb52bbec3b29a0c2e8b40
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-26 09:44:44 +0300

    Fix CORS

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const { handleErrors } = require('./handleErrors');
 const app = express();
+app.use(cors());
 
 app.use(express.static('public'));
 app.use(express.json());
