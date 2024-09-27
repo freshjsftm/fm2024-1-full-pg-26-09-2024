11dd581a996c7a7d4984263405cf25b6713aa522
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-27 09:29:43 +0300

    Refactor usersSlice

---
+++ client/src/store/helpers.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const decorateAsyncThunk = ({type, asyncThunk})=>{
+  return createAsyncThunk(
+    type,
+    async (values, thunkAPI) => {
+      try {
+        const {
+          data: { data },
+        } = await asyncThunk(values);
+        return data;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+  );
+}
+
+
+//eslint-disable-next-line
+export const pendingReducer = (state, action) => {
+  state.isPending = true;
+  state.error = null;
+};
+
+export const rejectedReducer = (state, action) => {
+  state.isPending = false;
+  state.error = action.payload;
+};
--- client/src/store/usersSlice.js
+++ client/src/store/usersSlice.js
@@ -1,33 +1,16 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { getAllUsers, postUser } from '../api';
+import { decorateAsyncThunk, pendingReducer, rejectedReducer } from './helpers';
 
-export const createUser = createAsyncThunk(
-  'users/createUser',
-  async (values, thunkAPI) => {
-    try {
-      const {
-        data: { data },
-      } = await postUser(values);
-      return data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
+export const createUser = decorateAsyncThunk({
+  type: 'users/createUser',
+  asyncThunk: postUser,
+});
 
-export const getUsers = createAsyncThunk(
-  'users/getUsers',
-  async ({ page, amount }, thunkAPI) => {
-    try {
-      const {
-        data: { data },
-      } = await getAllUsers({ page, amount });
-      return data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
+export const getUsers = decorateAsyncThunk({
+  type: 'users/getUsers',
+  asyncThunk: getAllUsers,
+});
 
 const usersSlice = createSlice({
   name: 'users',
@@ -40,29 +23,22 @@ const usersSlice = createSlice({
     //sort
   },
   extraReducers: (builder) => {
-    //eslint-disable-next-line
-    builder.addCase(createUser.pending, (state, action) => {
-      state.isPending = true;
-    });
+    builder.addCase(createUser.pending, pendingReducer);
+    builder.addCase(getUsers.pending, pendingReducer);
+
+    builder.addCase(createUser.rejected, rejectedReducer);
+    builder.addCase(getUsers.rejected, rejectedReducer);
+
     builder.addCase(createUser.fulfilled, (state, action) => {
       state.isPending = false;
+      state.error = null;
       state.users.push(action.payload);
     });
-    builder.addCase(createUser.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
-    //eslint-disable-next-line
-    builder.addCase(getUsers.pending, (state, action) => {
-      state.isPending = true;
-    });
+
     builder.addCase(getUsers.fulfilled, (state, action) => {
       state.isPending = false;
+      state.error = null;
       state.users = action.payload;
-    });
-    builder.addCase(getUsers.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
     });
   },
 });
